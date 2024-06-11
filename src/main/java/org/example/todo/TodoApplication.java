cc55c46e443e364e04398e238a8cd646a348e021
YOUNYUKYUNG <yyk40572576@gmail.com>
2024-06-11 13:34:58 +0900

    update

---
+++ src/main/java/org/example/todo/TodoApplication.java
@@ -0,0 +1,29 @@
+package org.example.todo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    @Value("${DB_USERNAME}")
+    private String dbUsername;
+
+    @Value("${DB_PASSWORD}")
+    private String dbPassword;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            System.out.println("DB_USERNAME: " + dbUsername);
+            System.out.println("DB_PASSWORD: " + dbPassword);
+        };
+    }
+}
---
+++ src/main/java/org/example/todo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.todo.repository;
+
+import org.example.todo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
